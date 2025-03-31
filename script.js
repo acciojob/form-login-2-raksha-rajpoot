3079d623c18c4adc8360a30847aefd45ea7cb5c2
raksha-rajpoot <rakraj654@gmail.com>
2025-03-31 17:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+   document.getElementById("loginForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+            
+            const firstName = document.getElementById("firstName").value;
+            const lastName = document.getElementById("lastName").value;
+            const phoneNumber = document.getElementById("phoneNumber").value;
+            const email = document.getElementById("email").value;
+            
+            alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`);
+        });
